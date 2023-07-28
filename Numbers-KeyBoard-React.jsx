e00a01d1a6861a5eec8310b68dd43f87b50ea279
Ouali Mohammed <118451360+mhmdouali06@users.noreply.github.com>
2023-07-28 10:18:58 +0100

    Update Numbers-KeyBoard-React.jsx

--- Numbers-KeyBoard-React.jsx
+++ Numbers-KeyBoard-React.jsx
@@ -15,7 +15,7 @@ const style = {
   p: 4,
 };
 
-const KeyBoard = ({ open, handleClose, setData, data }) => {
+const KeyBoard = ({ openKeyboard, handleCloseKey, setData, data }) => {
   const handleNumberClick = (number) => {
     setData((prevData) => prevData + number);
   };
@@ -28,8 +28,8 @@ const KeyBoard = ({ open, handleClose, setData, data }) => {
   return (
     <div>
       <Modal
-        open={open}
-        onClose={handleClose}
+        open={openKeyboard}
+        onClose={handleCloseKey}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
@@ -37,52 +37,69 @@ const KeyBoard = ({ open, handleClose, setData, data }) => {
           <form>
             <div className="d-flex justify-content-end mb-2">
               <button
+                type="button"
                 className="btn btn-danger btn-close"
-                onClick={handleClose}
+                onClick={handleCloseKey}
               ></button>
             </div>
-            <div className="row d-flex gap-2 d-md-flex justify-content-md-end mb-2 pe-3">
+            <div className="row d-flex  justify-content-center mb-2">
+              <input
+                type="text"
+                className="form-control"
+                readOnly
+                value={data}
+                id=""
+              />
+            </div>
+            <div className="row d-flex gap-2 d-md-flex justify-content-end mb-2 pe-3">
               <button
+                type="button"
                 className="btn btn-primary col-3"
                 onClick={(e) => handleDelete()}
               >
                 <FiDelete />
               </button>
             </div>
-            <div className="row d-flex gap-2 d-md-flex justify-content-md-around mb-2">
+            <div className="row d-flex gap-2 d-md-flex justify-content-around mb-2">
               <button
+                type="button"
                 className="btn btn-primary col-3"
                 onClick={() => handleNumberClick("1")}
               >
                 1
               </button>
               <button
+                type="button"
                 className="btn btn-primary col-3"
                 onClick={() => handleNumberClick("2")}
               >
                 2
               </button>
               <button
+                type="button"
                 className="btn btn-primary col-3"
                 onClick={() => handleNumberClick("3")}
               >
                 3
               </button>
             </div>
-            <div className="row d-flex gap-2 d-md-flex justify-content-md-around mb-2">
+            <div className="row d-flex gap-2 d-md-flex justify-content-around mb-2">
               <button
+                type="button"
                 className="btn btn-primary col-3"
                 onClick={() => handleNumberClick("4")}
               >
                 4
               </button>
               <button
+                type="button"
                 className="btn btn-primary col-3"
                 onClick={() => handleNumberClick("5")}
               >
                 5
               </button>
               <button
+                type="button"
                 className="btn btn-primary col-3"
                 onClick={() => handleNumberClick("6")}
               >
@@ -90,44 +107,59 @@ const KeyBoard = ({ open, handleClose, setData, data }) => {
               </button>
             </div>
 
-            <div className="row d-flex gap-2 d-md-flex justify-content-md-around mb-2">
+            <div className="row d-flex gap-2 d-md-flex justify-content-around mb-2">
               <button
+                type="button"
                 className="btn btn-primary col-3"
                 onClick={() => handleNumberClick("7")}
               >
                 7
               </button>
               <button
+                type="button"
                 className="btn btn-primary col-3"
                 onClick={() => handleNumberClick("8")}
               >
                 8
               </button>
               <button
+                type="button"
                 className="btn btn-primary col-3"
                 onClick={() => handleNumberClick("9")}
               >
                 9
               </button>
             </div>
-            <div className="row d-flex justify-content-md-around">
+            <div className="row d-flex gap-2 d-md-flex justify-content-around mb-2">
               <button
+                type="button"
                 className="btn btn-primary col-3"
                 onClick={() => handleNumberClick(".")}
               >
                 ,
               </button>
               <button
+                type="button"
                 className="btn btn-primary col-3"
                 onClick={() => handleNumberClick("0")}
               >
                 0
               </button>
               <button
+                type="button"
                 className="btn btn-danger col-3"
                 onClick={() => setData("")}
               >
                 C
+              </button>
+            </div>
+            <div className="row d-flex justify-content-start mt-3 ps-3">
+              <button
+                type="button"
+                className="btn btn-primary col-3"
+                onClick={handleCloseKey}
+              >
+                Valider
               </button>
             </div>
           </form>
@@ -138,8 +170,8 @@ const KeyBoard = ({ open, handleClose, setData, data }) => {
 };
 
 KeyBoard.propTypes = {
-  open: PropTypes.bool.isRequired,
-  handleClose: PropTypes.func.isRequired,
+  openKeyboard: PropTypes.bool.isRequired,
+  handleCloseKey: PropTypes.func.isRequired,
 };
 
 export default KeyBoard;
